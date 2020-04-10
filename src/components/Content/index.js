b09da2e3089687c2e91bfb23fc99a178702f42af
Sarah B <smblair87@gmail.com>
2020-04-10 07:10:12 -0700

    Added some media queries

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,7 +4,7 @@ import './style.css';
 const Content = (props) => {
   return (
     <div className='card'>
-      <img className='card-image' src={props.data.hdurl} alt='hi' />
+      <img className='card-image' src={props.data.url} alt='hi' />
       <div className='card-content'>
         <h3 className='card-title'> {props.data.title} </h3>
         <p className='card-bio'>{props.data.explanation}</p>
